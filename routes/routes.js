d7f8d4c71c73f3321ea9e29e9110fb21890fc9dd
Maksim Kapustin <makskapustin90@gmail.com>
2022-11-29 22:17:01 +0400

    chore: add eslint and prettier packages

--- routes/routes.js
+++ routes/routes.js
@@ -1,133 +1,133 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
+
 const router = express.Router();
-const fileMulter = require('../middleware/file')
 const path = require('path');
+const fileMulter = require('../middleware/file');
+
 class Book {
-    constructor(title = '', description='', authors='', favorite = '', fileCover = '', fileName = '', fileBook, id = uuidv4()) {
-      this.id = id,
-      this.title = title,
-      this.description = description,
-      this.authors = authors,
-      this.favorite = favorite,
-      this.fileCover = fileCover,
-      this.fileName = fileName,
-      this.fileBook = fileBook
-    }
+  constructor(
+    title = '',
+    description = '',
+    authors = '',
+    favorite = '',
+    fileCover = '',
+    fileName = '',
+    fileBook,
+    id = uuidv4(),
+  ) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.authors = authors;
+    this.favorite = favorite;
+    this.fileCover = fileCover;
+    this.fileName = fileName;
+    this.fileBook = fileBook;
   }
-  
-  const store = {
-    books: [],
+}
+
+const store = {
+  books: [],
+};
+
+router.get('/', (_, res) => {
+  const { books } = store;
+  res.json(books);
+});
+
+router.get('/:id', (req, res) => {
+  const { books } = store;
+  const { id } = req.params;
+  const idx = books.findIndex((item) => item.id === id);
+
+  if (idx !== -1) {
+    res.json(books[idx]);
+  } else {
+    res.status(404);
+    res.json('данные не найдены');
   }
+});
 
-  
-  router.get('/', (_, res) => {
-    const { books } = store;
-    res.json(books);
-  });
-  
-  router.get('/:id', (req, res) => {
-    const { books } = store;
-    const { id } = req.params;
-    const idx = books.findIndex(item => item.id === id);
-  
-    if (idx !== -1) {
-      res.json(books[idx])
-    } else {
-      res.status(404);
-      res.json('данные не найдены')
-    }
-  });
+router.get('/:id/download', (req, res) => {
+  const { books } = store;
+  const { id } = req.params;
+  const idx = books.findIndex((item) => item.id === id);
 
-  router.get('/:id/download', (req, res) => {
-    const { books } = store;
-    const { id } = req.params;
-    const idx = books.findIndex(item => item.id === id);
-
-    if (idx !== -1) {
-      res.download(path.join(__dirname, `../public/books/${books[idx].fileBook}`))
-    } else {
-      res.status(404);
-      res.json('данные не найдены')
-    }
-  });
-  
-  router.post('/', fileMulter.single('book'), (req, res) => {
-    const { books } = store;
-    const {
-      title, 
-      description, 
-      authors, 
-      favorite, 
-      fileCover, 
-      fileName,
-    } = req.body;
-    
-    let fileInfo = '';
-
-    if (req.file) {
-      fileInfo = req.file?.filename;
-    }
-  
-    const newBook = new Book(
-      title, 
-      description, 
-      authors, 
-      favorite, 
-      fileCover, 
-      fileName,
-      fileBook = fileInfo,
+  if (idx !== -1) {
+    res.download(
+      path.join(__dirname, `../public/books/${books[idx].fileBook}`),
     );
-  
-    books.push(newBook);
-    res.json(newBook);
-  });
-  
-  router.put('/:id', (req, res) => {
-    const { books } = store;
-    const { id } = req.params;
-  
-    const {
-      title, 
-      description, 
-      authors, 
-      favorite, 
-      fileCover, 
-      fileName
-    } = req.body;
-  
-    const idx = books.findIndex(item => item.id === id);
-  
-    if (idx !== -1) {
-      books[idx] = {
-        ...books[idx],
-        title, 
-        description, 
-        authors, 
-        favorite, 
-        fileCover, 
-        fileName,
-      }
-      res.json(books[idx]);
-    } else {
-      res.status(404);
-      res.json('данные не найдены')
-    }
-  });
-  
-  router.delete('/:id', (req, res) => {
-    const { books } = store;
-    const { id } = req.params;
-  
-    const idx = books.findIndex(item => item.id === id);
-  
-    if (idx !== -1) {
-      books.splice(idx, 1);
-      res.json('ok')
-    } else {
-      res.status(404);
-      res.json('данные не найдены')
-    }
-  });
+  } else {
+    res.status(404);
+    res.json('данные не найдены');
+  }
+});
+
+router.post('/', fileMulter.single('book'), (req, res) => {
+  const { books } = store;
+  const { title, description, authors, favorite, fileCover, fileName } =
+    req.body;
+
+  let fileInfo = '';
+
+  if (req.file) {
+    fileInfo = req.file?.filename;
+  }
+
+  const newBook = new Book(
+    title,
+    description,
+    authors,
+    favorite,
+    fileCover,
+    fileName,
+    fileInfo,
+  );
+
+  books.push(newBook);
+  res.json(newBook);
+});
+
+router.put('/:id', (req, res) => {
+  const { books } = store;
+  const { id } = req.params;
+
+  const { title, description, authors, favorite, fileCover, fileName } =
+    req.body;
+
+  const idx = books.findIndex((item) => item.id === id);
+
+  if (idx !== -1) {
+    books[idx] = {
+      ...books[idx],
+      title,
+      description,
+      authors,
+      favorite,
+      fileCover,
+      fileName,
+    };
+    res.json(books[idx]);
+  } else {
+    res.status(404);
+    res.json('данные не найдены');
+  }
+});
+
+router.delete('/:id', (req, res) => {
+  const { books } = store;
+  const { id } = req.params;
+
+  const idx = books.findIndex((item) => item.id === id);
+
+  if (idx !== -1) {
+    books.splice(idx, 1);
+    res.json('ok');
+  } else {
+    res.status(404);
+    res.json('данные не найдены');
+  }
+});
 
 module.exports = router;
